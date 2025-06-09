9dd480274459385e1528adf8ff5d52d57c32df71
ddutta <dibyendu.dutta08@gmail.com>
2025-06-09 23:18:59 +0530

    Text to speech engine

--- PA.py
+++ PA.py
@@ -2,3 +2,8 @@ import requests
 import gradio as gr
 import speech_recognition as sr
 import pyttsx3
+ollama_url = "http://localhost:11434/api/chat/generate"
+#text to speech engine
+engine = pyttsx3.init()
+def ai_assistant(imput_text):
+    prompt = f"Answer the following question:\n {imput_text}"
