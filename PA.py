088e5c5dd9188d0d764ce6690f899cd5f3847a8b
ddutta <dibyendu.dutta08@gmail.com>
2025-06-10 21:54:01 +0530

    Adding base logic

--- PA.py
+++ PA.py
@@ -7,3 +7,18 @@ ollama_url = "http://localhost:11434/api/chat/generate"
 engine = pyttsx3.init()
 def ai_assistant(imput_text):
     prompt = f"Answer the following question:\n {imput_text}"
+    payload = {
+        "model":"deepseek-r1:1.5b",
+        "prompt": prompt,
+        "stream": False,
+    }
+    response = requests.post(ollama_url, json=payload)
+    if response.status_code == 200:
+        ai_response = response.json().get("response", "I don't know the answer to that.")
+    # Convert the response to speech
+        engine.say(ai_response)
+        engine.runAndWait()
+        return ai_response
+    else:
+        return "Error: Unable to get a response from the AI model."
+# Function to handle speech input using voice command function
