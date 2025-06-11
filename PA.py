72efc2f324d3d69373d9489a5b79236a82b988b3
ddutta <dibyendu.dutta08@gmail.com>
2025-06-11 09:45:04 +0530

    Fixed failures

--- PA.py
+++ PA.py
@@ -2,7 +2,7 @@ import requests
 import gradio as gr
 import speech_recognition as sr
 import pyttsx3
-ollama_url = "http://localhost:11434/api/chat/generate"
+ollama_url = "http://localhost:11434/api/generate"
 #text to speech engine
 engine = pyttsx3.init()
 def ai_assistant(imput_text):
